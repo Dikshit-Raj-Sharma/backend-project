6187b4beed0cddc80e0ff15e7a79905b7f1ef88c
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2026-05-22 09:29:41 +0000

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+/*
+approach 1
+import express from "express";
+const app= express();
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+*/
