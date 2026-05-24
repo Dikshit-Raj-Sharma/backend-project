a11f00ed366c7bdf996cb081ae3946a5848a3103
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2026-05-24 19:51:35 +0000

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,5 +16,9 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.route.js";
 
+//routes declaration
+app.use("/api/v1/users",userRouter);
 export {app};   
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
 .then(()=>{
