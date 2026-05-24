dab5db65864306ad097515a346564afea7e079b5
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2026-05-24 19:01:05 +0000

    used multer and couldinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next) {
    if(!this.isModified("password")) return next();
-   this.password=bcrypt.hash(this.password,10);
+   this.password= await bcrypt.hash(this.password,10);
    next();
 });
 
@@ -61,8 +61,8 @@ userSchema.methods.generateAccessToken=function(){
    return jwt.sign(
       {
          _id: this._id,
-         email=this.email,
-         fullname=this.fullname,
+         email:this.email,
+         fullname:this.fullname,
       },
       process.env.ACCESS_TOKEN_SECRET,
       {
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,14 +8,14 @@ class ApiError extends Error {
     ){
         super(message);
         this.statusCode = statusCode;
-        this.stack = statck;
+        this.stack = stack;
         this.data=null;
         this.message=message;
         this.success=false;
         this.errors=errors
 
-        if(statck){
-            this.stack = statck;
+        if(stack){
+            this.stack = stack;
         }else{
             Error.captureStackTrace(this, this.constructor);
         }
